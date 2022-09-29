b0930d891ef90264c02449362598cc29cf6d049c
Ramiro Torres <ramixalex@hotmail.com>
2022-09-29 11:15:05 +0200

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="#"><img src="https://i.imgur.com/MaJ2sMs.png" alt="" /></a>
+                <a href="#"><img src="https://i.imgur.com/wGeU9D5.png" alt="" /></a>
+                <a href="#"><img src="https://i.imgur.com/ojZSy4O.png" alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
