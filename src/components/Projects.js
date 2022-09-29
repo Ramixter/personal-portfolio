40f7a0076c1eb0bd600e4b322e6d23eabaccc367
Ramiro Torres <ramixalex@hotmail.com>
2022-09-29 10:46:18 +0200

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -92,7 +92,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src="https://imgur.com/a/ZPx9pWg"></img>
+      <img className="background-image-right" src={colorSharp2}></img>
     </section>
   )
 }
