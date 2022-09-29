912eeaa4f901b9c4583d52163e7dcf6bd498ddb8
Ramiro Torres <ramixalex@hotmail.com>
2022-09-29 10:46:25 +0200

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -66,7 +66,7 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src="https://i.imgur.com/6wF55lH.png" alt="Header Img"/>
+                  <img src="https://i.postimg.cc/BQj13pY5/header-img.png" alt="Header Img"/>
                 </div>}
             </TrackVisibility>
           </Col>
