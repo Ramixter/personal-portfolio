c16598820bb3b58a741b2bf34e23e84c16d884f7
Ramiro Torres <ramixalex@hotmail.com>
2022-09-29 10:09:16 +0200

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,7 @@ import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "https://raw.githubusercontent.com/Ramixter/personal-portfolio/3645b0965c1bd87e4c5c4dadc63d0caf365668e6/src/assets/img/header-img.svg";
+import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
 
