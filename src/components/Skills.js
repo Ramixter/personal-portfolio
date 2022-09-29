a35e3c61f10941e69731f2a77b13e58b6dd7ca61
Ramiro Torres <ramixalex@hotmail.com>
2022-09-29 10:46:16 +0200

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,9 +3,9 @@ import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "https://i.imgur.com/YrOicEX.png";
-import arrow2 from "https://i.imgur.com/L9sMedX.png";
-import colorSharp from "https://i.imgur.com/2xEmKuo.png"
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
   const responsive = {
@@ -58,7 +58,7 @@ export const Skills = () => {
                 </div>
             </div>
         </div>
-        <img className="background-image-left" src="https://imgur.com/a/1TOyiNP" alt="Image" />
+        <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
   )
 }
