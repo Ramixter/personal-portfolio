396586ab6b152946f5ea541e49ed7c02af9afad8
Ramiro Torres <ramixalex@hotmail.com>
2022-09-29 10:14:57 +0200

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -58,7 +58,7 @@ export const Skills = () => {
                 </div>
             </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+        <img className="background-image-left" src="https://imgur.com/a/1TOyiNP" alt="Image" />
     </section>
   )
 }
